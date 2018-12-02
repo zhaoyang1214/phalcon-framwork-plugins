9bb1a27ff2fb4f77fba0f8c8ed1e810f10418436
zhaoyang <1134856531@qq.com>
2018-12-02 19:33:49 +0800

    add dbprofiler plugin

---
+++ src/DbProfiler.php
@@ -0,0 +1,44 @@
+<?php
+/** 
+ * @desc SQL语句性能分析插件 
+ */
+namespace PhalconPlugins;
+
+use Phalcon\Events\Event;
+use Phalcon\Mvc\User\Plugin;
+
+class DbProfiler extends Plugin
+{
+
+    /**
+     * 执行sql语句前执行
+     *
+     * @param Event $event
+     *            事件
+     * @param $connection 数据库连接            
+     */
+    public function beforeQuery(Event $event, $connection)
+    {
+        $this->profiler->startProfile($connection->getSQLStatement(), $connection->getSqlVariables(), $connection->getSQLBindTypes());
+    }
+
+    /**
+     * 执行sql语句前执行
+     *
+     * @param Event $event
+     *            事件
+     * @param $connection 数据库连接            
+     */
+    public function afterQuery(Event $event, $connection)
+    {
+        $profiler = $this->profiler;
+        $profiler->stopProfile();
+        $profile = $profiler->getLastProfile();
+        $sql = $profile->getSQLStatement();
+        $params = $profile->getSqlVariables();
+        $params = json_encode($params, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $executeTime = $profile->getTotalElapsedSeconds();
+        $profiler->reset();
+        $this->di->getLogger()->info("$sql $params $executeTime");
+    }
+}
